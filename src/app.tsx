338cb1724cc463447bd7701d3b103b074e2d5c03
nathancamolez-dev <nathan.camolez@unesp.br>
2024-09-11 17:13:10 -0300

    feat: all interface is done , empty goals and goal with empty conclusions

--- src/app.tsx
+++ src/app.tsx
@@ -1,112 +1,13 @@
-import logo from './assets/logo-inorbit.svg'
-import letsStart from './assets/lets-start-illustration.svg'
-import { Plus, X } from 'lucide-react'
-import { Button } from './components/ui/button'
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogTitle,
-  DialogTrigger,
-} from './components/ui/dialog'
-import { Label } from './components/ui/label'
-import { Input } from './components/ui/input'
-import {
-  RadioGroup,
-  RadioGroupIndicator,
-  RadioGroupItem,
-} from './components/ui/radio-group'
+import { Dialog } from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
+// import { EmptyGoals } from './components/empty-goals'
 export function App() {
   return (
     <Dialog>
-      <div className="h-screen flex flex-col items-center justify-center gap-8">
-        <img src={logo} alt="in.orbit" />
-        <img src={letsStart} alt="lets Start" />
-        <p className="text-zinc-50 leading-relaxed max-w-80 text-center">
-          Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
-          mesmo?
-        </p>
-
-        <DialogTrigger asChild>
-          <Button>
-            <Plus className="size-4" />
-            Cadastrar meta
-          </Button>
-        </DialogTrigger>
-      </div>
-      <DialogContent>
-        <div className="h-full flex flex-col gap-6">
-          <div className="flex flex-col gap-3">
-            <div className="flex items-center justify-between">
-              <DialogTitle>Cadastrar Meta</DialogTitle>
-              <DialogClose>
-                <X className="size-5 text-zinc-600 hover:text-zinc-100" />
-              </DialogClose>
-            </div>
-
-            <DialogDescription>
-              Adicione atividades que te fazem bem e que você quer continuar
-              praticando toda semana.
-            </DialogDescription>
-          </div>
-
-          <form action="" className="flex-1 flex flex-col justify-between">
-            <div className="flex flex-col gap-6">
-              <div className="flex flex-col gap-2">
-                <Label htmlFor="title">Qual a atividade?</Label>
-                <Input
-                  id="title"
-                  autoFocus
-                  placeholder="Praticar exercicios, meditar, etc..."
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <Label htmlFor="title">Quantas vezes na semana?</Label>
-                <RadioGroup>
-                  <RadioGroupItem value="1">
-                    <RadioGroupIndicator />
-                    <span className="text-zinc-300 text-sm font-medium leading-none">
-                      1x na semana
-                    </span>
-                    <span className="text-lg leading-none">🥱</span>
-                  </RadioGroupItem>
-                  <RadioGroupItem value="2">
-                    <RadioGroupIndicator />
-                    <span className="text-zinc-300 text-sm font-medium leading-none">
-                      2x na semana
-                    </span>
-                    <span className="text-lg leading-none">🙂</span>
-                  </RadioGroupItem>
-                  <RadioGroupItem value="3">
-                    <RadioGroupIndicator />
-                    <span className="text-zinc-300 text-sm font-medium leading-none">
-                      3x na semana
-                    </span>
-                    <span className="text-lg leading-none">😎</span>
-                  </RadioGroupItem>
-                  <RadioGroupItem value="4">
-                    <RadioGroupIndicator />
-                    <span className="text-zinc-300 text-sm font-medium leading-none">
-                      4x na semana
-                    </span>
-                    <span className="text-lg leading-none">😜</span>
-                  </RadioGroupItem>
-                </RadioGroup>
-              </div>
-            </div>
-
-            <div className="flex items-center gap-3">
-              <DialogClose asChild>
-                <Button variant="secondary" className="flex-1">
-                  Fechar
-                </Button>
-              </DialogClose>
-              <Button className="flex-1">Salvar</Button>
-            </div>
-          </form>
-        </div>
-      </DialogContent>
+      {/* <EmptyGoals /> */}
+      <Summary />
+      <CreateGoal />
     </Dialog>
   )
 }
